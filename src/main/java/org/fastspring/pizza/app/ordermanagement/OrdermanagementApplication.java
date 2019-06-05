890d092a7e9277680cd86375d6c27b0910baeea3
Anupam <anupam.cs@gmail.com>
2019-06-04 21:20:57 -0500

    New Project Created for Pizza Order Management Backend API.

---
+++ src/main/java/org/fastspring/pizza/app/ordermanagement/OrdermanagementApplication.java
@@ -0,0 +1,13 @@
+package org.fastspring.pizza.app.ordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fastspring/pizza/app/ordermanagement/OrdermanagementApplicationTests.java
@@ -0,0 +1,16 @@
+package org.fastspring.pizza.app.ordermanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrdermanagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
